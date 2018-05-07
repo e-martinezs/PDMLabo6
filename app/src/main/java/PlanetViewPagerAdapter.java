88b5343e24b1c9418644e141990ede4002ebe526
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-05-07 12:15:50 -0600

    Se implemento view pager adapter

---
+++ app/src/main/java/PlanetViewPagerAdapter.java
@@ -0,0 +1,37 @@
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by uca on 05-07-18.
+ */
+
+public class PlanetViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<String> titleList;
+    private List<Fragment> fragmentList;
+
+    public PlanetViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        titleList = new ArrayList<>();
+        fragmentList = new ArrayList<>();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    public void addItem(String title, Fragment fragment){
+        titleList.add(title);
+        fragmentList.add(fragment);
+    }
+}
